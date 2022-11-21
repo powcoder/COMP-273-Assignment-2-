1297859547b7b09857b844cb0c17c628613e70eb
powcoder <powcoder@163.com>
2022-11-21 15:10:31 +0800

    COMP 273 Assignment 2

---
+++ points.asm
@@ -0,0 +1,38 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+# This program calculates the slope and midpoint between two points in a plane
+
+	.data
+str_x1:
+	.asciiz "Enter x1: "
+str_y1:
+	.asciiz "Enter y1: "
+str_x2:
+	.asciiz "Enter x2: "
+str_y2:
+	.asciiz "Enter y2: "
+out_midpoint:
+	.asciiz "The midpoint is: "
+out_slope:
+	.asciiz "The slope is: "
+
+
+	.text	
+main:
+# YOUR CODE HERE
+
+
+
+
+
+
+
+
+
+
+
+# EXIT PROGRAM
+li $v0,10		#system call code for exit = 10
+syscall			#call operating sys : EXIT
